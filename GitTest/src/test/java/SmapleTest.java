10ac8f6c07474a46475214d0158edb7b86afa97d
anusha g p <anushagp5ee@gmail.com>
2026-02-18 11:08:04 -0500

    test

---
+++ GitTest/src/test/java/SmapleTest.java
@@ -0,0 +1,14 @@
+
+public class SmapleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String str = "anusha";
+ 
+        char[] a= str.toCharArray();
+        System.out.println(str);
+        for(int i=a.length-1; i>=0;i--)
+        	 System.out.print(a[i]);
+	}
+
+}
